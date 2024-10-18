faa7df04ace19d06702afe752f017b86a96eb807
Xylo-o <134782673+Xylo-o@users.noreply.github.com>
2024-10-18 18:10:43 +0200

    Update evil.sh
    Changed sleep delay + fixed small typo

--- evil.sh
+++ evil.sh
@@ -5,11 +5,11 @@ cd() {
 
     trap '' INT
 
-    say "Ooops I fucked up, forgot to lock my screen, wow. What I do now, what I do now. Maybe I should ask bocal for help. YES, I SHOULD DEFINITELY ASK BOCAL FOR HELP! Bocal, bocal, bocal - help, help, help. Fuck, fuck, fuck - I guess I'm stuck. No one can help, what in the name of duck. Okay, I stop it here." 
+    say "Ooops I fucked up, forgot to lock my screen, wow. What I do now, what I do now. Maybe I should ask bocal for help. YES, I SHOULD DEFINITELY ASK BOCAL FOR HELP! Bocal, bocal, bocal - help, help, help. Fuck, fuck, fuck - I guess I'm stuck. No one can help, what in the name of duck. Oh dear oh dear oh dear. Okay, I stop it here." 
    
-    sleep 5
+    sleep 3
 
-    say "Just kidding, there's more! I can't ever get bored! Script so good, you can't resist - I already hear germans talking "Verpiss". Actually, fuck this rhyming - I will be productive and stop whining. Thank you for the show, we ran out of clock. Next time - I will just use that fucking screen lock. "
+    say "Just kidding, there's more! I can't ever get bored! Script so good, you can't resist - I already hear germans talking - Verpiss. Actually, fuck this rhyming - I will be productive and stop whining. Thank you for the show, we ran out of clock. Next time - I will just use that fucking screen lock."
     
     trap - INT
 
