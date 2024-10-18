683d9ee42ffe077661ee90661ccd0ba503637cb7
Xylo-o <134782673+Xylo-o@users.noreply.github.com>
2024-10-18 17:41:23 +0200

    Create evil.sh
    1st version of the script

---
+++ evil.sh
@@ -0,0 +1,13 @@
+alias original_cd='builtin cd'
+
+cd() {
+    osascript -e 'set volume output volume 100'
+
+    trap '' INT
+
+    say "Ooops I fucked up, forgot to lock my screen, wow. What I do now, what I do now. Maybe I should ask bocal for help. YES, I SHOULD DEFINITELY ASK BOCAL FOR HELP! Bocal, bocal, bocal - help, help, help. Fuck, fuck, fuck - I guess I'm stuck. No one can help, what in the name of duck. Okay, I stop it here. [[slnc 5000]] Just kidding, there's more! I can't ever get bored! Script so good, you can't resist - I already hear germans talking "Verpiss". Actually, fuck this rhyming - I will be productive and stop whining. Thank you for the show, we ran out of clock. Next time - I will just use that fucking screen lock. "
+
+    trap - INT
+
+    original_cd "$@"
+}
